107fd048abc8072626016bdc4de10fe5c5c2fff5
kathiravanelango <ekathiravan2000@gmail.com>
2021-03-14 15:06:40 +0530

    edit profile

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from .views import indexView,loginView,signupView,logoutView,profileView
+from .views import indexView,loginView,signupView,logoutView,profileView,profileEditView
 
 urlpatterns = [
     path('', indexView,name="indexView"),
     path('login/',loginView,name="loginView" ),
     path('signup/',signupView,name="signupView" ),
     path('logout/',logoutView,name="logoutView" ),
-    path('profile/',profileView,name="profileView" )
+    path('profile/',profileView,name="profileView" ),
+    path('profile/edit/',profileEditView,name="profileEditView"),
 ]
--- users/views.py
+++ users/views.py
@@ -5,53 +5,75 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Profile
 from posts.models import Post
+from .forms import UserUpdateForm,ProfileUpdateForm
 
 def indexView(request):
-    if(request.user.is_authenticated):
-        posts = Post.objects.all().order_by('-date_posted')
-        return render(request,'users/index.html',{'posts':posts})
-    return render(request,'users/index.html')
+	if(request.user.is_authenticated):
+		posts = Post.objects.all().order_by('-date_posted')
+		return render(request,'users/index.html',{'posts':posts})
+	return render(request,'users/index.html')
 
 def loginView(request):
-    if(request.method == 'POST'):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            next = request.GET.get('next')
-            if(next):
-                return redirect(next)
-            return redirect('indexView')
-        else:
-           messages.warning(request,f'Username and Password combination does not match')
+	if(request.method == 'POST'):
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			next = request.GET.get('next')
+			if(next):
+				return redirect(next)
+			return redirect('indexView')
+		else:
+		   messages.warning(request,f'Username and Password combination does not match')
 
-    return render(request,'users/login.html')
+	return render(request,'users/login.html')
 
 def logoutView(request):
-    logout(request)
-    return redirect('indexView')
+	logout(request)
+	return redirect('indexView')
 
 def signupView(request):
-    if(request.method == 'POST'):
-        username = request.POST.get('username')
-        firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = User.objects.create_user(
-            username=username,
-            first_name=firstname,
-            last_name=lastname,
-            email=email,
-            password=password)
-        profile = Profile.objects.create(user=user)
-        profile.save()
-        return redirect('loginView')
-    return render(request,'users/signup.html')        
+	if(request.method == 'POST'):
+		username = request.POST.get('username')
+		firstname = request.POST.get('firstname')
+		lastname = request.POST.get('lastname')
+		email = request.POST.get('email')
+		password = request.POST.get('password')
+		user = User.objects.create_user(
+			username=username,
+			first_name=firstname,
+			last_name=lastname,
+			email=email,
+			password=password)
+		profile = Profile.objects.create(user=user)
+		profile.save()
+		return redirect('loginView')
+	return render(request,'users/signup.html')        
 
 @login_required
 def profileView(request):
-    posts = request.user.post_set.all().order_by('-date_posted')
-    print(posts)
-    return render(request,'users/profile.html',{'posts'  : posts })
+	posts = request.user.post_set.all().order_by('-date_posted')
+	return render(request,'users/profile.html',{'posts'  : posts })
+
+@login_required
+def profileEditView(request):
+	if request.method == 'POST':
+		user_form = UserUpdateForm(request.POST,instance=request.user)
+		profile_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+		# instance is passsed to form because it has to know what User and Profile it has to update
+		# if it is not passed it will try to create an another new object for User and Profile
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request,f'Profile Successfully Updated')
+			return redirect('/profile')		
+
+	user_form = UserUpdateForm(instance=request.user)
+	profile_form = ProfileUpdateForm(instance=request.user.profile)
+	context = { 
+		'user_form':user_form,
+		'profile_form':profile_form
+	}
+	return render(request,'users/profileEdit.html',context)
+	
